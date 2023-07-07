e3e393ed3b23fe0c265e47e0b54d9eca19d2fa37
Gagana <gagana.dhanakoti@gmail.com>
2023-07-07 15:06:04 -0400

    Initial Commit

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+import { requirePropFactory } from "@mui/material";
+
+import React from 'react'
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+    return (
+        <main>
+            <Outlet/>
+
+        </main>
+    )
+}
+
+export default Layout
